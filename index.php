008580a98f4e6cc66701899f604a5a828a49cc6c
walmordouglas <walmordouglas@hotmail.com>
2016-04-05 21:00:35 -0300

    Inicio

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Curso de Inglês</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/estilos.css" />
+  </head>
+  <body>
+    <div class="container">
+        <!-- Banner topo -->
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Curso de Inglês</h1>
+            </div>
+        </div>
+        <!-- Barra de Menu -->
+        <div class="row">
+            <div class="col-md-12">
+                <nav class="navbar navbar-default">
+                  <div class="container-fluid">
+                      <div class="navbar-header">
+                          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#identificador-menu" aria-expanded="false">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                          </button>
+                          <a class="navbar-brand" href="/index.php">Página Inicial</a>
+                      </div>
+                      <div class="collapse navbar-collapse" id="identificador-menu">
+                        <ul class="nav navbar-nav">
+                            <li><a href="#">Cadastro de Módulo</a></li>
+                            <li class="active"><a href="#">Cadastro de Aluno</a></li>
+                        </ul>
+                      </div>
+                  </nav>
+          </div>
+        </div>
+        <!-- Conteúdo -->
+        <div class="row">
+            <div class="col-md-12">
+                Conteúdo
+            </div>
+        </div>
+    </div>
+    <script src="bootstrap/js/bootstrap.min.js" />
+  </body>
+</html>
+<?php
